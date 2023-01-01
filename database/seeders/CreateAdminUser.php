15cedf5c994942259ba9339a4798dfa8804f6323
Alex Brea <bgalejandro97@gmail.com>
2023-01-01 18:30:47 +0100

    Filament basic config

---
+++ database/seeders/CreateAdminUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CreateAdminUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create(
+            [
+                'name'     => 'test',
+                'email'    => 'test@test.com',
+                'password' => Hash::make( 'user' ),
+            ]
+        );
+    }
+}
